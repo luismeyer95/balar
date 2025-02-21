3a2a524b44c012c279359fc047c7ce49d0da2ec3
luismeyer95 <luismeyer95@gmail.com>
2025-02-21 19:07:16 +0100

    moved main test case setup into tests

--- src/index.ts
+++ src/index.ts
@@ -13,113 +13,11 @@ class UpdateIssues {
   }
 }
 
-export async function processBulkRequest(
-  requests: UpdateRequest[],
-): Promise<Map<number, UpdateIssues>> {
-  const issues: Map<number, UpdateIssues> = new Map();
-
-  // validate budget not 0
-  for (const request of requests) {
-    if (request.budget === 0) {
-      if (!issues.has(request.id)) {
-        issues.set(request.id, new UpdateIssues());
-      }
-      issues.get(request.id)!.errors.push("budget should be greater than 0");
-    }
-  }
-
-  requests = requests.filter((request) => !issues.has(request.id));
-
-  // fetch current budgets and map them into new model for processing
-  const currentBudgetById = await getCurrentBudgets(
-    requests.map((request) => request.id),
-  );
-  const oldAndNewBudgetsById = new Map(
-    requests.map((request) => [
-      request.id,
-      [currentBudgetById.get(request.id)!, request.budget],
-    ]),
-  );
-
-  // validate budget not lowered
-  for (const [requestId, [oldBudget, newBudget]] of oldAndNewBudgetsById) {
-    if (oldBudget > newBudget) {
-      if (!issues.has(requestId)) {
-        issues.set(requestId, new UpdateIssues());
-      }
-      issues.get(requestId)!.errors.push("budget must not be lowered");
-    }
-  }
-
-  requests = requests.filter((request) => !issues.has(request.id));
-
-  // run the update
-  const updatedBudgets = await updateBudgets(requests);
-
-  for (const request of requests) {
-    if (!updatedBudgets.get(request.id)) {
-      if (!issues.has(request.id)) {
-        issues.set(request.id, new UpdateIssues());
-      }
-      issues.get(request.id)!.errors.push("budget update failed");
-    }
-  }
-
-  return issues;
-}
-
-export async function processor(
-  request: UpdateRequest,
-  use: {
-    getCurrentBudgets: (id: number) => Promise<number>;
-    updateBudgets: (request: UpdateRequest) => Promise<boolean>;
-  },
-): Promise<UpdateIssues | null> {
-  const issues = new UpdateIssues();
-
-  if (request.budget === 0) {
-    issues.errors.push("budget should be greater than 0");
-    return issues;
-  }
-
-  const currentBudget = await use.getCurrentBudgets(request.id);
-  if (request.budget < currentBudget) {
-    issues.errors.push("budget must not be lowered");
-    return issues;
-  }
-
-  const updatedBudget = await use.updateBudgets(request);
-  if (!updatedBudget) {
-    issues.errors.push("budget update failed");
-    return issues;
-  }
-
-  const newBudget = await use.getCurrentBudgets(request.id);
-
-  return null;
-}
-
 // How to handle the API of operations
 // - either leave untouched, still with bulk semantincs. each request processor must retrieve its own data from the result.
 // - or transform into scalar APIs. lib is tasked to retrieve the queried data from the result.
 // For the latter, the scalar result must be resolvable.
 // - either the result is keyed by input, or by an id in the input
-
-export async function processBulkRequest2(
-  requests: UpdateRequest[],
-): Promise<Map<number, UpdateIssues>> {
-  const plan = new BulkyPlan(processor, {
-    getCurrentBudgets,
-    updateBudgets: { fn: updateBudgets, keySelector: (r) => r.id },
-  });
-
-  const result: Map<number, UpdateIssues> = await plan.run(
-    requests,
-    (r) => r.id,
-  );
-
-  return result;
-}
 
 type Handler<In, Out> = {
   fn: (request: In[]) => Promise<Map<number, Out>>;
@@ -127,7 +25,7 @@ type Handler<In, Out> = {
   keySelector: (request: In) => number;
 };
 
-class BulkyPlan {
+export class BulkyPlan {
   getCurrentBudgets: Handler<number, number>;
   updateBudgets: Handler<UpdateRequest, boolean>;
 
@@ -236,20 +134,4 @@ class BulkyPlan {
 
     return resultsByKey;
   }
-}
-
-// External APIs
-
-async function getCurrentBudgets(
-  requestIds: number[],
-): Promise<Map<number, number>> {
-  console.log("getCurrentBudgets", requestIds);
-  return new Map(requestIds.map((requestId) => [requestId, requestId * 500]));
-}
-
-async function updateBudgets(
-  requests: UpdateRequest[],
-): Promise<Map<number, boolean>> {
-  console.log("updateBudgets", requests);
-  return new Map(requests.map((request) => [request.id, request.id % 4 !== 0]));
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,11 +1,73 @@
-import { processBulkRequest, processBulkRequest2 } from "../src/index";
+import { BulkyPlan } from "../src/index";
 
 test("it works", async () => {
+  interface UpdateRequest {
+    id: number;
+    budget: number;
+  }
+
+  class UpdateIssues {
+    errors: string[];
+
+    constructor() {
+      this.errors = [];
+    }
+  }
+
+  // Processing function
+
+  async function processor(
+    request: UpdateRequest,
+    use: {
+      getCurrentBudgets: (id: number) => Promise<number>;
+      updateBudgets: (request: UpdateRequest) => Promise<boolean>;
+    },
+  ): Promise<UpdateIssues | null> {
+    const issues = new UpdateIssues();
+
+    if (request.budget === 0) {
+      issues.errors.push("budget should be greater than 0");
+      return issues;
+    }
+
+    const currentBudget = await use.getCurrentBudgets(request.id);
+    if (request.budget < currentBudget) {
+      issues.errors.push("budget must not be lowered");
+      return issues;
+    }
+
+    const updatedBudget = await use.updateBudgets(request);
+    if (!updatedBudget) {
+      issues.errors.push("budget update failed");
+      return issues;
+    }
+
+    return null;
+  }
+
+  // External APIs
+
+  async function getCurrentBudgets(
+    requestIds: number[],
+  ): Promise<Map<number, number>> {
+    console.log("getCurrentBudgets", requestIds);
+    return new Map(requestIds.map((requestId) => [requestId, requestId * 500]));
+  }
+
+  async function updateBudgets(
+    requests: UpdateRequest[],
+  ): Promise<Map<number, boolean>> {
+    console.log("updateBudgets", requests);
+    return new Map(
+      requests.map((request) => [request.id, request.id % 4 !== 0]),
+    );
+  }
+
   const requests = [
-    { id: 1, budget: 1000 },
-    { id: 2, budget: 0 },
-    { id: 3, budget: 1 },
-    { id: 4, budget: 3000 },
+    { id: 1, budget: 1000 }, // success
+    { id: 2, budget: 0 }, // fail: can't have 0
+    { id: 3, budget: 1 }, // fail: can't lower
+    { id: 4, budget: 3000 }, // fail: divisible by 4
   ];
 
   const expected = new Map([
@@ -14,6 +76,14 @@ test("it works", async () => {
     [4, { errors: ["budget update failed"] }],
   ]);
 
-  const issues = await processBulkRequest2(requests);
+  const plan = new BulkyPlan(processor, {
+    getCurrentBudgets,
+    updateBudgets: { fn: updateBudgets, keySelector: (r) => r.id },
+  });
+
+  const issues: Map<number, UpdateIssues> = await plan.run(
+    requests,
+    (r) => r.id,
+  );
   expect(issues).toEqual(expected);
 });
